8518518c0dc9f4cd8849c5902c1c579c1648e584
danrox9 <danrox.9@gmail.com>
2023-04-17 21:30:18 +0200

    Se hace la eliminacion de cuenta de usuario, falta pedirle si esta seguro

--- PopFun/app/src/main/java/com/example/popfun/AjustesFragment.java
+++ PopFun/app/src/main/java/com/example/popfun/AjustesFragment.java
@@ -1,10 +1,14 @@
 package com.example.popfun;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,9 +16,20 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 
 public class AjustesFragment extends Fragment {
+
+    Button eliminarcuenta;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,13 +40,41 @@ public class AjustesFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
+        eliminarcuenta = view.findViewById(R.id.eliminarcuenta);
 
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         ActionBar actionBar = activity.getSupportActionBar();
         Drawable customBackground = getResources().getDrawable(R.drawable.fondo_degradado);
         actionBar.setBackgroundDrawable(customBackground);
 
+        borrarusuarioaut();
+
+
+    }
+
+    public void borrarusuarioaut(){
+        eliminarcuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // Borrar el usuario actual y todos sus datos
+                    user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(getActivity(), MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                Log.d("TAG", "Borrado exitoso");
+                            } else {
+                                Log.d("TAG", "Error al borrar");
+                            }
+                        }
+                    }
+                    );
+                }
+            }
+        });
     }
 }
--- PopFun/app/src/main/java/com/example/popfun/HomeActivity.java
+++ PopFun/app/src/main/java/com/example/popfun/HomeActivity.java
@@ -100,6 +100,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
                 break;
+            case R.id.nav_ajustes:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AjustesFragment()).commit();
+                break;
             case R.id.nav_logout:
                 Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(HomeActivity.this, MainActivity.class);
